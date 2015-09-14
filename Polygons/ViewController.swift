33393589002a02f43e4ae508445cc1ec12c527be
Navya Rayala <Navya@Navya-MB-P.local>
2015-09-14 18:57:14 -0500

    Initial Commit

---
+++ Polygons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Polygons
+//
+//  Created by Navya Rayala on 9/14/15.
+//  Copyright (c) 2015 Kamal Dandamudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
