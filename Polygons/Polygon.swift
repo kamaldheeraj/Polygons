a936a42fd517979cbba34afb39d9cdbc668f04ef
Navya Rayala <Navya@Navya-MB-P.local>
2015-09-14 19:16:36 -0500

    Added willSet methods to min and max sides

--- Polygons/Polygon.swift
+++ Polygons/Polygon.swift
@@ -9,7 +9,31 @@
 import Foundation
 
 class Polygon{
-  var minSides = 3
-  var maxSides = 12
+    var minSides: Int = 3 {
+        willSet(newMinSides){
+            if newMinSides>maxSides{
+            minSides=maxSides
+            }
+            else if newMinSides<3{
+            minSides=3
+            }
+            else{
+            minSides=newMinSides
+            }
+        }
+    }
+    var maxSides: Int = 12 {
+        willSet(newMaxSides){
+            if newMaxSides<minSides{
+            maxSides=minSides
+            }
+            else if newMaxSides>12{
+            maxSides=12
+            }
+            else{
+            maxSides=newMaxSides
+            }
+        }
+    }
     
 }
