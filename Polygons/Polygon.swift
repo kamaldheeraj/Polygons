13193aa3e877c79853e288a65563755596f8fb29
Navya Rayala <Navya@Navya-MB-P.local>
2015-09-14 19:03:42 -0500

    Created Polygon swift file and Polygon class

---
+++ Polygons/Polygon.swift
@@ -0,0 +1,15 @@
+//
+//  Polygon.swift
+//  Polygons
+//
+//  Created by Navya Rayala on 9/14/15.
+//  Copyright (c) 2015 Kamal Dandamudi. All rights reserved.
+//
+
+import Foundation
+
+class Polygon{
+  var minSides = 3
+  var maxSides = 12
+    
+}
